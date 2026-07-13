4635bd5cc61cfffb1ef08a1c6a3ea19c66f6aac7
Silviu-Ionuț Boangiu <ionut.boangiu@itsyscom.com>
2026-07-13 21:41:59 +0300

    simplify logger interface to only define Warning

--- guardian.go
+++ guardian.go
@@ -33,14 +33,6 @@ type Option func(*GuardianLocker)
 
 // logger defines the logging interface required.
 type logger interface {
-	Alert(string) error
-	Close() error
-	Crit(string) error
-	Debug(string) error
-	Emerg(string) error
-	Err(string) error
-	Info(string) error
-	Notice(string) error
 	Warning(string) error
 }
 
@@ -270,12 +262,4 @@ func (gl *GuardianLocker) unlockWithReference(refID string) (lkIDs []string) {
 
 type nopLogger struct{}
 
-func (nopLogger) Alert(string) error   { return nil }
-func (nopLogger) Close() error         { return nil }
-func (nopLogger) Crit(string) error    { return nil }
-func (nopLogger) Debug(string) error   { return nil }
-func (nopLogger) Emerg(string) error   { return nil }
-func (nopLogger) Err(string) error     { return nil }
-func (nopLogger) Info(string) error    { return nil }
-func (nopLogger) Notice(string) error  { return nil }
 func (nopLogger) Warning(string) error { return nil }
